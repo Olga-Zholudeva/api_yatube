d4eee1b0deddf4a666d30d6e16f60e5b57276d18
Ольга Жолудева <joludeva.olga@yandex.ru>
2022-11-27 19:53:48 +0300

    Правки по итогам 1-го ревью

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
-DEBUG = str(os.getenv('DEBUG'))
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
